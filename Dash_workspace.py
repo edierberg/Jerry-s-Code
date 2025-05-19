ef13cf63011a7c3d4f025c45cb51707846ef2057
Dash <dseals@exeter.edu>
2025-05-19 08:47:11 -0400

    Wheel changes and general outline.

--- Dash_workspace.py
+++ Dash_workspace.py
@@ -8,6 +8,39 @@
     width of arrow head = 10
     speed of wheel
 '''
+
+import sys
+
+import pygame
+import pygame.locals
+
 Width_Arrow_Head = 10
 Speed_Wheel = 20 
-Radius_Wheel = 50
+Radius_Wheel = 200
+
+def Wheel(radius, width, height, screen):
+    pygame.draw.circle(screen, "#ff0000", (width/2, height/2 - radius), radius, 0)
+    pygame.display.flip()
+    return 
+
+def Spin(Speed):
+    return
+
+def detection(Width_Arrows):
+    return
+
+def main():
+    fps = 60
+    fps_clock = pygame.time.Clock()
+    width, height = 1000, 1000
+    screen = pygame.display.set_mode((width, height))
+
+    Wheel(Radius_Wheel, width, height, screen)
+
+    while True:
+        screen.fill("#000000")
+        fps_clock.tick(fps)
+
+
+if __name__ == "__main__":
+    main()
