42f86c725f74d4ca89f354e1a5370f02d510489a
JerryMa2028 <jerryma2028@gmail.com>
2025-05-26 08:33:11 -0400

    update

--- Jerry_workspace.py
+++ Jerry_workspace.py
@@ -4,7 +4,8 @@ import tkinter as tk
 import pygame
 import pygame.locals
 import math
-
+import random
+import time
 '''
 minimum viable project:
 Spinning Wheel (wheel spinning around fixed point) 
@@ -22,7 +23,7 @@ a. Can restart and shows high scores
 '''
 
 pygame.init()
-fps = 240
+fps = 60
 fpsClock = pygame.time.Clock()
 width, height = 900, 900 
 screen = pygame.display.set_mode((width, height))
@@ -30,28 +31,29 @@ pygame.display.set_caption("Needle Shooting Game")
 
 center = (450, 450)
 radius = 150
-center_color = (255, 0, 255) 
-edge_color = (0, 0, 255)      
+center_color = (x, y, z) 
+edge_color = (a, b, c)
+
 
 # Game loop.
-for y in range(center[1] - radius, center[1] + radius):
-    for x in range(center[0] - radius, center[0] + radius):
-        dx = x - center[0]
-        dy = y - center[1]
-        distance = math.sqrt(dx**2 + dy**2)
+
+
+# Event loop to keep the window open
+running =True
+while running:
+    for y in range(center[1] - radius, center[1] + radius):
+        for x in range(center[0] - radius, center[0] + radius):
+            dx = x - center[0]
+            dy = y - center[1]
+            distance = math.sqrt(dx**2 + dy**2)
         if distance <= radius:
             t = distance / radius
             color = tuple(int(center_color[i] + (edge_color[i] - center_color[i]) * t) for i in range(3))
             screen.set_at((x, y), color)
 
-pygame.display.flip()
-
-# Event loop to keep the window open
-running =True
-while running:
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
 pygame.quit()
-    
