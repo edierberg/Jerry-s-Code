9c5d1ba6c1628182d68406f6daabd98bdd9d22ed
JerryMa2028 <jerryma2028@gmail.com>
2025-05-30 20:31:23 -0400

    comit

--- Jerry_workspace.py
+++ Jerry_workspace.py
@@ -29,7 +29,8 @@ width, height = 900, 900
 picture = "circle.png"
 screen = pygame.display.set_mode((width, height))
 original_image = pygame.image.load(picture).convert_alpha()
-rect = original_image.get_rect(center=(450, 450))
+center_x =450
+center_y = 450
 angle = 0
 clock = pygame.time.Clock()
 last_change = pygame.time.get_ticks()
@@ -41,6 +42,10 @@ target_speed = 1.0
 last_speed_change =time.time()
 transition_start_speed = 1.0
 in_transition = False
+sb=0
+
+last_y_change =time.time()
+in_transform = False
 
 running = True
 while running:
@@ -61,18 +66,30 @@ while running:
         else:
             current_speed = transition_start_speed + (target_speed - transition_start_speed) * transition_progress
 
-#    if current_time%30=0 and not in_transition:
-#        target_speed = random.uniform(-1.0, 1.0)
-#        transition_start_speed = current_speed
-#        transition_start_time = current_time
-#        in_transition = True
-#        last_speed_change = current_time
+    if current_time - last_y_change > 30 and not in_transform:
+        in_transform = True
+        last_y_change = current_time
+        transform_start_time =current_time
 
+    if in_transform:
+        transform_progress = (current_time - transform_start_time)
+        if transform_progress <= 2:
+            sb = -0.2
+        elif 2<transform_progress <= 6:
+            sb = 0.2
+        elif 6<transform_progress <= 8:
+            sb = -0.2
+        else:
+            sb =0
+            in_transform = False
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     angle += current_speed
+    center_y += sb
+    rect = original_image.get_rect(center=(center_x, center_y))
     angle %= 360
     rotated_image = pygame.transform.rotate(original_image, -angle) 
     rotated_rect = rotated_image.get_rect(center=rect.center)
